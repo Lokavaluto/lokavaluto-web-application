eb037e9c4571fcabcf28234c0097b0e6cec28b06
Seddik <SeddikKadi>
2025-02-20 18:25:43 +0100

    fix: add maximum safe amount value for all transactions

--- src/components/MoneyTransaction.vue
+++ src/components/MoneyTransaction.vue
@@ -227,15 +227,14 @@
             )
           return
         }
-
         const amountStrRaw = this.$refs.amountRequested.value
         const amountStr = this.amount.toString()
         // XXXvlab: this is the maximum size of a XXXX.YY that
         // is safely converted to a number in javascript. (We
         // can garantee that what the user typed in is
         // eauivalent to what we get in the code.
-        const maxValue = Number.MAX_SAFE_INTEGER / 2 ** 7
-        if (this.amount > maxValue) {
+        const maxValue = 2 ** 46
+        if (this.amount >= maxValue) {
           this.errors.amount = this.$gettext(
             "Amount to send is too large (<= %{ maxValue })",
             { maxValue }
